68a8c1db1fbd4f533c5863b5df99510581706d24
aaronharinck <aaron.harinck@student.howest.be>
2019-11-27 09:41:58 +0100

    added parseitems

--- src/js/components/player/Playlist.js
+++ src/js/components/player/Playlist.js
@@ -5,10 +5,17 @@ class Playlist extends EventEmitter2 {
   constructor($el) {
     super({}); // roep de constructor op van EventEmitter2
     this.$el = $el;
+    this._items = this.parseItemsFromEl();
+    console.log(this._items);
   }
 
   parseItemsFromEl(){
-
+    const $links = this.$el.querySelectorAll(`.playlist__link`);
+    const urls  = [];
+    $links.forEach(link => {
+      urls.push({url: link.href});
+    });
+    return urls;
   }
 
 }
